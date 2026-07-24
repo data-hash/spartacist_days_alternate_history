55c70a3d0f399da1d29f79009c607778d4d1a59f
data-hash <danmberlin@gmail.com>
2026-07-24 16:00:09 -0400

    status changes

--- out/html/game.js
+++ out/html/game.js
@@ -183,10 +183,13 @@
   // TODO: have some code for tabbed sidebar browsing.
   window.updateSidebar = function() {
       $('#qualities').empty();
+      var statusScene = dendryUI.game.scenes["status"];
       var scene = dendryUI.game.scenes[window.statusTab];
+      dendryUI.dendryEngine._runActions(statusScene.onArrival);
       dendryUI.dendryEngine._runActions(scene.onArrival);
       var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+      dendryUI.dendryEngine._runActions(scene.onDisplay);
   };
 
   window.changeTab = function(newTab, tabId) {
