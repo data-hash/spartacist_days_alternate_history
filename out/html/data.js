d65573a51e2a4f1f8b1d59a849a0231448199374
data-hash <danmberlin@gmail.com>
2026-09-09 01:26:29 -0400

    Final CSS Tweaks

--- out/html/data.js
+++ out/html/data.js
@@ -151,124 +151,124 @@ const tooltipList = [
     searchString: "KPD",
     tooltipColor: "#700000",
     explanationText:
-      "<img src=img/logos/kpd_logo.svg> KPD</br> Communist Party of Germany </br></br> Kommunistische Partei Deutschlands",
+      "<img src=img/logos/kpd_logo.svg> Communist Party of Germany </br></br> Kommunistische Partei Deutschlands",
   },
 
   {
     searchString: "USPD",
     tooltipColor: "#b90903",
     explanationText:
-      "<img src=img/logos/uspd_logo.svg> USPD</br> Independent Social Democratic Party of Germany Deutschlands </br></br> Unabhängige Sozialdemokratische Partei Deutschlands",
+      "<img src=img/logos/uspd_logo.svg> Independent Social Democratic Party of Germany Deutschlands </br></br> Unabhängige Sozialdemokratische Partei Deutschlands",
   },
 
   {
     searchString: "SAPD",
     tooltipColor: "#C40000",
     explanationText:
-      "<img src=img/logos/SAPD_logo.png> SAPD</br> Socialist Workers' Party of Germany </br></br> Sozialistische Arbeiterpartei Deutschlands",
+      "<img src=img/logos/SAPD_logo.png> Socialist Workers' Party of Germany </br></br> Sozialistische Arbeiterpartei Deutschlands",
   },
 
   {
     searchString: "SPD",
     tooltipColor: "#E3000F",
     explanationText:
-      "<img src=img/logos/SPD_logo_2.png> SPD</br> Social Democratic Party of Germany </br></br> Sozialdemokratische Partei Deutschlands",
+      "<img src=img/logos/SPD_logo_2.png> Social Democratic Party of Germany </br></br> Sozialdemokratische Partei Deutschlands",
   },
 
   {
     searchString: "MSPD",
     tooltipColor: "#E3000F",
     explanationText:
-      "<img src=img/logos/SPD_logo_2.png> MSPD</br> Majority Social Democratic Party of Germany </br></br> Mehrheitssozialdemokratische Partei Deutschlands",
+      "<img src=img/logos/SPD_logo_2.png> Majority Social Democratic Party of Germany </br></br> Mehrheitssozialdemokratische Partei Deutschlands",
   },
 
   {
     searchString: "DHP",
     tooltipColor: "#333FFF",
     explanationText:
-      " DHP</br> German-Hanoverian Party </br></br> Deutsch-Hannoversche Partei",
+      " German-Hanoverian Party </br></br> Deutsch-Hannoversche Partei",
   },
 
   {
     searchString: "ELP",
     tooltipColor: "#000",
     explanationText:
-      " ELP</br> Alsace-Lorraine Party </br></br> Elsass-Lothringen Partei",
+      " Alsace-Lorraine Party </br></br> Elsass-Lothringen Partei",
   },
 
   {
     searchString: "BB",
     tooltipColor: "#008B45",
     explanationText:
-      " BB</br> Bavarian Peasants' League </br></br> Bayerischer Bauernbund",
+      " Bavarian Peasants' League </br></br> Bayerischer Bauernbund",
   },
 
   {
     searchString: "PP",
     tooltipColor: "#D49797",
-    explanationText: " PP</br> Polish Party </br></br> Polenpartei",
+    explanationText: " Polish Party </br></br> Polenpartei",
   },
 
   {
     searchString: "DP",
     tooltipColor: "#CE1126",
-    explanationText: " DP</br> Danish Party </br></br> Dänische Partei",
+    explanationText: " Danish Party </br></br> Dänische Partei",
   },
 
   {
     searchString: "Others",
     tooltipColor: "#a0a0a0",
     explanationText:
-      " Independents </br> Agrarian League, Independent Conservatives, etc.",
+      " Independents </br></br> Agrarian League, Independent Conservatives, etc.",
   },
 
   {
     searchString: "OTHER",
     tooltipColor: "#a0a0a0",
     explanationText:
-      " Independents </br> Agrarian League, Independent Conservatives, etc.",
+      " Independents </br></br> Agrarian League, Independent Conservatives, etc.",
   },
 
   {
     searchString: "FVP",
     tooltipColor: "#d4b402",
     explanationText:
-      "<img src=img/logos/fvp_logo.svg> FVP</br> Progressive People's Party </br></br> Fortschrittliche Volkspartei",
+      "<img src=img/logos/fvp_logo.svg> Progressive People's Party </br></br> Fortschrittliche Volkspartei",
   },
 
   {
     searchString: "DDP",
     tooltipColor: "#d4b402",
     explanationText:
-      "<img src=img/logos/weimar_flag.svg> DDP</br> German Democratic Party </br></br> Deutsche Demokratische Partei",
+      "<img src=img/logos/weimar_flag.svg> German Democratic Party </br></br> Deutsche Demokratische Partei",
   },
 
   {
     searchString: "Z",
     tooltipColor: "#000000",
     explanationText:
-      "<img src=img/logos/z_flag.svg> Z</br> German Centre Party </br></br> Deutsche Zentrumspartei",
+      "<img src=img/logos/z_flag.svg> German Centre Party </br></br> Deutsche Zentrumspartei",
   },
 
   {
     searchString: "BVP",
     tooltipColor: "#69A2BE",
     explanationText:
-      "<img src=img/logos/bvp_logo.jpeg> BVP</br> Bavarian People's Party </br></br> Bayerische Volkspartei",
+      "<img src=img/logos/bvp_logo.jpeg> Bavarian People's Party </br></br> Bayerische Volkspartei",
   },
 
   {
     searchString: "NLP",
     tooltipColor: "#98C22C",
     explanationText:
-      "<img src=img/logos/nlp_logo.png> NLP</br> National Liberal Party </br></br> Nationalliberale Partei",
+      "<img src=img/logos/nlp_logo.png> National Liberal Party </br></br> Nationalliberale Partei",
   },
 
   {
     searchString: "DVP",
     tooltipColor: "#D5AC27",
     explanationText:
-      "<img src=img/logos/dvp_logo.svg> DVP</br> German People's Party </br></br> Deutsche Volkspartei",
+      "<img src=img/logos/dvp_logo.svg> German People's Party </br></br> Deutsche Volkspartei",
   },
 
   {
@@ -289,35 +289,35 @@ const tooltipList = [
     searchString: "DKP",
     tooltipColor: "#0781E5",
     explanationText:
-      "<img src=img/logos/imperial_flag.svg> DKP</br> German Conservative Party </br></br> Deutschkonservative Partei",
+      "<img src=img/logos/imperial_flag.svg> German Conservative Party </br></br> Deutschkonservative Partei",
   },
 
   {
     searchString: "DNVP",
     tooltipColor: "#3E88B3",
     explanationText:
-      "<img src=img/logos/dnvp_logo.svg> DNVP</br> German National People's Party </br></br> Deutschnationale Volkspartei",
+      "<img src=img/logos/dnvp_logo.svg> German National People's Party </br></br> Deutschnationale Volkspartei",
   },
 
   {
     searchString: "DvP",
     tooltipColor: "#8B5A2B",
     explanationText:
-      "<img src=img/logos/freikorps_flag.png> DvP</br> German Völkisch Party </br></br> Deutschvölkische Partei",
+      "<img src=img/logos/freikorps_flag.png> German Völkisch Party </br></br> Deutschvölkische Partei",
   },
 
   {
     searchString: "DSP",
     tooltipColor: "#8B5A2B",
     explanationText:
-      "<img src=img/logos/freikorps_flag.png> DvP</br> German Völkisch Party </br></br> Deutschvölkische Partei",
+      "<img src=img/logos/freikorps_flag.png> German Völkisch Party </br></br> Deutschvölkische Partei",
   },
 
   {
     searchString: "NSDAP",
     tooltipColor: "#964B00",
     explanationText:
-      " NSDAP</br> National Socialist German Workers' Party </br></br> Nationalsozialistische Deutsche Arbeiterpartei",
+      " National Socialist German Workers' Party </br></br> Nationalsozialistische Deutsche Arbeiterpartei",
   },
 
   {
